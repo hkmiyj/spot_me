fb8a8bc37a765cd43466a8a75dcffbf6913d416f
Ariff Hakimi <66932774+hkmiyj@users.noreply.github.com>
2022-08-29 11:58:09 +0800

    test

--- lib/view/registration.dart
+++ lib/view/registration.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:spot_me/route/route.dart' as route;
 import 'package:spot_me/service/firebase_authentication.dart';
 
